432d8612872948d8face051b7db749449f4dd985
wan pu <imwanpu@gmail.com>
2023-07-24 13:58:50 -0400

    update vm_config_network.sh

--- utils/vm_config_network.sh
+++ utils/vm_config_network.sh
@@ -6,15 +6,19 @@
 base_uuid="92e576d5-f199-4e5c-895c-a3f5f9ae821"
 new_uuid="$((RANDOM % 100000000))-$((RANDOM % 10000))-$((RANDOM % 10000))-$((RANDOM % 10000))-$((RANDOM % 100000000000))"
 new_ip="10.0.0."$1
+new_hostname="h"$1
 
 sed_1_arg="/UUID=/s/.*/UUID=${new_uuid}/"
 sed $sed_1_arg /etc/sysconfig/network-scripts/ifcfg-ens160 -in
-sed_2_arg="/IPADDR=/s/.*/IPADDR=$new_ip"
-sed $sed_2_arg /etc/sysconfig/netword-scripts/ifcfg-ens160 -in
+sed_2_arg="/IPADDR=/s/.*/IPADDR=$new_ip/"
+sed $sed_2_arg /etc/sysconfig/network-scripts/ifcfg-ens160 -in
+hostname $new_hostname
 
 echo "new uuid is: $new_uuid"
 echo "new ip is: $new_ip"
-
+echo "new hostname is: "
 systemctl restart network
 systemctl disable firewalld
 systemctl stop firewalld
+ip addr
+hostname
