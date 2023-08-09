efe61399fec153969b9dd157ad3e5f8648e95f98
wan pu <imwanpu@gmail.com>
2023-08-09 10:34:40 +0800

    update cluster_nslookup

--- cluster_nslookup/longge2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in {1..600}; do
-    IP=$(hostname -I | awk '{print $1}')
-    echo Test baidu.com From $IP
-    echo $i && date && time nslookup baidu.com 114.114.114.114 | grep time
-    echo Test zhihu.com From $IP
-    echo $i && date && time nslookup zhihu.com 114.114.114.114 | grep time
-    sleep 3
-done
