a2c2dbdd20f1ccae463083c9f4a69b90af6d34ff
wan pu <imwanpu@gmail.com>
2023-08-09 10:34:15 +0800

    update cluster_nslookup

--- cluster_nslookup/parser.py
+++ cluster_nslookup/parser.py
@@ -1,11 +1,16 @@
 #!/bin/python3
 
-file_path = '/root/cluster-scripts/cluster_nslookup/serial_nslookup_tmp.txt'
+import re
+import os
+
+hosts_results_dir = '/tmp/serial_nslookup_data/'
+
 
 class answer:
     def __init__(self):
         self.name = ""
         self.address = ""
+
 
 class domain:
     def __init__(self):
@@ -17,42 +22,99 @@ class domain:
         self.sys = ""
         self.date = ''
 
-class remote_result:
+
+class host_result:
     def __init__(self) -> None:
         self.host_ip = ''
-        self.result: [domain] = []
+        self.results: [domain] = []
 
-result: [domain] = []
+    def print_hr(self) -> None:
+        for domain in self.results:
+            for answer in domain.answers:
+                print(
+                    f'{domain.server:<18}{self.host_ip:<18}{answer.name:<28}{answer.address:<18}{domain.real:<10}')
 
-# TODO 抽 function
-with open(file_path) as f:
-    line = f.readline()
-    while line:
-        kv = line.split()
-        print(kv, type(kv))
-        if 'Server:' in kv:
-            d = domain()
-            result.append(d)
-            result[-1].server = kv[1]
-        elif 'Address:' in kv:
-            result[-1].address = kv[1]
-        elif 'Name:' in kv:
-            a = answer()
-            result[-1].answers.append(a)
-            result[-1].answers[-1].name = kv[1]
-            line = f.readline()
-            kv = line.split()
-            result[-1].answers[-1].address = kv[1]
-        elif 'real' in kv:
-            result[-1].real = kv[1]
-        elif 'user' in kv:
-            result[-1].user = kv[1]
-        elif 'sys' in kv:
-            result[-1].sys = kv[1]
-            line = f.readline()
-            result[-1].date = line.strip()
+
+class hosts_results:
+    def __init__(self) -> None:
+        self.results: [host_result] = []
+
+    def print_hsrs(self) -> None:
+        for hr in self.results:
+            hr.print_hr()
+
+    def print_table_head(self) -> None:
+        answer_name = 'Domain'
+        answer_address = 'Address'
+        domain_server = 'DNS'
+        domain_address = ''
+        domain_real = 'Real'
+        domain_user = ''
+        domain_sys = ''
+        domain_date = ''
+        host_result_host_ip = 'Host'
+        print(
+            f'{domain_server:<18}{host_result_host_ip:<18}{answer_name:<28}{answer_address:<18}{domain_real:<10}')
+
+
+def parse_serial_nslookup_host_result(file_path: str) -> host_result:
+
+    hr = host_result()
+
+    hr.host_ip = re.search(
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', file_path).group()
+
+    with open(file_path) as f:
         line = f.readline()
-  
-print(result)
+        while line:
+            kv = line.split()
+            # print(kv, type(kv))
+            if 'Server:' in kv:
+                d = domain()
+                hr.results.append(d)
+                hr.results[-1].server = kv[1]
+            elif 'Address:' in kv:
+                hr.results[-1].address = kv[1]
+            elif 'Name:' in kv:
+                a = answer()
+                hr.results[-1].answers.append(a)
+                hr.results[-1].answers[-1].name = kv[1]
+                line = f.readline()
+                kv = line.split()
+                hr.results[-1].answers[-1].address = kv[1]
+            elif 'real' in kv:
+                hr.results[-1].real = kv[1]
+            elif 'user' in kv:
+                hr.results[-1].user = kv[1]
+            elif 'sys' in kv:
+                hr.results[-1].sys = kv[1]
+                line = f.readline()
+                hr.results[-1].date = line.strip()
+            line = f.readline()
+    return hr
 
-print(f'date\t')
+
+def get_all_file_paths(directory: str) -> list:
+    file_paths = []  # 存储所有文件的绝对路径
+
+    # 遍历目录及子目录中的所有文件
+    for root, directories, files in os.walk(directory):
+        for file_name in files:
+            # 获取文件的绝对路径
+            file_path = os.path.abspath(os.path.join(root, file_name))
+            file_paths.append(file_path)
+
+    return file_paths
+
+
+def parse_hosts_results(host_results_dir: str) -> hosts_results:
+    hsrs = hosts_results()
+    for serial_nslookup_host_result in get_all_file_paths(hosts_results_dir):
+        hsrs.results.append(parse_serial_nslookup_host_result(
+            serial_nslookup_host_result))
+    return hsrs
+
+
+hsrs = parse_hosts_results(hosts_results_dir)
+hsrs.print_table_head()
+hsrs.print_hsrs()
--- cluster_nslookup/serial_nslookup.sh
+++ cluster_nslookup/serial_nslookup.sh
@@ -4,7 +4,7 @@
 
 domains_file="/tmp/ansible/domains.txt"
 dns="114.114.114.114"
-tmp_file="/tmp/ansible/serial_nslookup_tmp.txt"
+tmp_file="/tmp/ansible/serial_nslookup_host_result.txt"
 
 domains=$(cat ${domains_file})
 if [ -f "${tmp_file}" ]; then
@@ -18,13 +18,15 @@ for domain in ${domains}; do
     # nslookup_result=$(time nslookup "${domain}" ${dns} 2>&1)
     # echo "${nslookup_result}"
     (time nslookup "${domain}" ${dns} && date) >>"${tmp_file}" 2>&1
-    awk '
-        {count[$1] += $2};
-        END{
-            for (key in count) {printf("%s\t\t", key)}
-            printf("\n")
-            for (key in count) {printf("%s\t\t", count[key])}
-            printf("\n")
-        }
-    ' ${tmp_file}
+    # awk '
+    #     {count[$1] += $2};
+    #     END{
+    #         for (key in count) {printf("%s\t\t", key)}
+    #         printf("\n")
+    #         for (key in count) {printf("%s\t\t", count[key])}
+    #         printf("\n")
+    #     }
+    # ' ${tmp_file}
 done
+
+exit 0
